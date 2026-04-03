05d398ba55994ff47e6565b816002abaa356442b
M-ADARSHCHARY <adarshchary.05@gmail.com>
2026-04-03 12:05:10 +0530

    refactor: restructure server with src directory and implement environment-based CORS security

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import connectDB from '../DB/db.js';
-import taskRoutes from '../routes/route.tasks.js';
-import userRoutes from '../routes/route.users.js';
+import connectDB from './DB/db.js';
+import taskRoutes from './routes/route.tasks.js';
+import userRoutes from './routes/route.users.js';
 
 dotenv.config();
 const app = express();
@@ -12,7 +12,9 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: process.env.CLIENT_URI || "http://localhost:5173",
+}));
 app.use(express.json());
 app.use("/api", taskRoutes);
 app.use("/api", userRoutes);
